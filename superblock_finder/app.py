692e15ee8d3e986b328f81e0eb5a8816a96d06f1
Denny Mattern <denny.mattern@pd-g.de>
2023-01-10 10:20:07 +0100

    running add_job

--- superblock_finder/app.py
+++ superblock_finder/app.py
@@ -1,6 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from psycopg import connect
+from os import environ
+from _types import Region, Coord
 
 app = FastAPI()
 
@@ -10,6 +12,33 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/region")
+def add_job(roi: Region):
+    with connect(dbname=environ["POSTGRES_DATABASE"], host=environ["POSTGRES_HOST"], user=environ["POSTGRES_USER"],
+                 password=environ["POSTGRES_PASSWORD"]) as con:
+        with con.cursor() as cur:
+            sql = "CREATE TABLE IF NOT EXISTS jobs (" \
+                  " id serial NOT NULL PRIMARY KEY," \
+                  " roi geometry NOT NULL," \
+                  " name text, " \
+                  " created_at timestamp WITHOUT TIME ZONE default current_timestamp" \
+                  ");"
+            cur.execute(sql)
+            con.commit()
+
+            if len(roi.coords) < 3:
+                return HTTPException(status_code=500, detail="No valid polygon given. You need to provide at least 3 points.")
+
+            coords = roi.coords + [roi.coords[0]]
+            polygon = "POLYGON((" + ",".join([f"{point.lng} {point.lat}" for point in coords]) + "))"
+
+            sql = f"INSERT INTO jobs (name, roi) " \
+                  f"VALUES (" \
+                  f"    '{roi.name}', " \
+                  f"    ST_SetSRID(ST_GeomFromText('{polygon}'), 4326)" \
+                  f")" \
+                  f"RETURNING *"
+            cur.execute(sql)
+            result = cur.fetchall()
+
+    return {"coords": roi.coords, "name": roi.name, "return": result}
